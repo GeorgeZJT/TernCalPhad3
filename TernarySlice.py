fe03204ae811fc0a2ef8cd9c458742abd4b36c63
GeorgeZJT <zhgeorge@umich.edu>
2025-08-21 01:18:29 -0400

    first try

--- TernarySlice.py
+++ TernarySlice.py
@@ -1,22 +1,30 @@
 import matplotlib.pyplot as plt
 import time
+import os
 from pycalphad import Database, ternplot
 from pycalphad import variables as v
 
 db_al_cu_y = Database('Al-Cu-Y.tdb')
 comps = ['AL', 'CU', 'Y', 'VA']
 phases = list(db_al_cu_y.phases.keys())
-conds = {v.T: 1500, v.P:101325, v.X('AL'): (0,1,0.015), v.X('Y'): (0,1,0.015)}
+
+# Ensure the output directory exists
+output_dir = 'output_images'
+os.makedirs(output_dir, exist_ok=True)
+
+for temperature in range (1770, 2500, 10):
+    conds = {v.T: temperature, v.P:101325, v.X('AL'): (0,1,0.015), v.X('Y'): (0,1,0.015)}
 
 
-# Set higher DPI for the figure
-plt.rcParams['figure.dpi'] = 300
-start_time = time.time()
-ax = ternplot(db_al_cu_y, comps, phases, conds, x=v.X('AL'), y=v.X('Y'), label_nodes=True)
-end_time = time.time()
-print(f"Elapsed time: {end_time - start_time:.3f} seconds")
+    # Set higher DPI for the figure
+    plt.rcParams['figure.dpi'] = 300
+    start_time = time.time()
+    ax = ternplot(db_al_cu_y, comps, phases, conds, x=v.X('AL'), y=v.X('Y'), label_nodes=True)
+    end_time = time.time()
+    print(f"Elapsed time for T = {temperature}: {end_time - start_time:.3f} seconds")
 
 
 
-plt.savefig('ternary_diagram_high_dpi.png', dpi=300)
-plt.show()
+    plt.savefig(os.path.join(output_dir, f'T={temperature}.png'), dpi=300)
+    # plt.show() 
+    plt.clf()
